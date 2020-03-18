0a8328ec53c0648b7ff49e3982fb5f8c17ae21ac
evgutich <rozetko@yahoo.com>
2020-03-18 19:21:03 +0300

    first commit

--- src/main/java/by/it/academy/scientificactivity/controller/EmployeesController.java
+++ src/main/java/by/it/academy/scientificactivity/controller/EmployeesController.java
@@ -5,15 +5,19 @@ import by.it.academy.scientificactivity.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping(value = "/employees")
 public class EmployeesController {
 
     @Autowired
     private EmployeeService employeeService;
 
+    @GetMapping
     public String employeeList(Model model){
         List<Employee> allEmployees = employeeService.getAllEmployees();
         model.addAttribute("employees", allEmployees);
--- src/main/java/by/it/academy/scientificactivity/model/Employee.java
+++ src/main/java/by/it/academy/scientificactivity/model/Employee.java
@@ -1,21 +1,22 @@
 package by.it.academy.scientificactivity.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity
+@Table(name = "EMPLOYEE")
 public class Employee {
 
     @Id
@@ -24,6 +25,9 @@ public class Employee {
     private String name;
     private String patronymic;
     private String surname;
+    @Column(name = "date_of_birth")
+//    @DateTimeFormat(iso = DateTimeFormatter)
+//    @JsonFormat(pattern = "YYYY-MM-dd")
     private LocalDate dateOfBirth;
     private String department;
 
